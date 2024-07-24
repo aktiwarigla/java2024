88f4b197dc4055cfc56b54647d8c37ac152b74d0
aktiwarigla <abhishek.kumar@gla.ac.in>
2024-07-24 17:32:33 +0530

    fist test

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.test.hello.Hello;
+public class Main {
+    public static void main(String[] args) {
+Hello h = new Hello();
+h.h();
+
+
+    }
+}
---
+++ src/com/test/hello/Hello.java
@@ -0,0 +1,9 @@
+package com.test.hello;
+
+public class Hello {
+
+
+    public void h() {
+        System.out.print("Hello Java");
+    }
+}
